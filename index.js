88ce1a774155a83436cfc5cecb612d799d923183
Alex Guanipatin <sebas5950@gmail.com>
2022-04-29 16:17:15 -0400

    commit tree fee04d70853e9ad42127e4ada43c4d57f842d5bb

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+const cards = ["Guadalupe", "Ollie", "Aki"]
+
+function writeCards(cards, eventName){
+  let newArray = [];
+  for (let i = 0; i < cards.length; i++){
+   newArray.push(`Thank you, ${cards[i]}, for the wonderful ${eventName} gift!`);
+     }
+  return newArray;
+}
+
+function countDown(num){
+  let i = num
+  while (i >= 0){
+    console.log(i--);
+  }
+}
